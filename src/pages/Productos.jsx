46d0014ad30023008d4df986d555eded2a13be56
Marlon Rodriguez Rivera <110213632+marlon7piri@users.noreply.github.com>
2024-04-29 15:44:27 -0500

    add new hasrouter 2

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,22 +1,13 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
-import { DataContext } from "../components/data/DataProvider";
+import { DataContext, DataProvider } from "../components/data/DataProvider";
 
 import CardProductos from "../components/CardProductos";
 import { Outlet } from "react-router-dom";
 import { getProductRequest } from "../components/data/api";
 
 export default function Productos() {
-  const [productos, setProductos] = useState([]);
-
-  const fetchStrapi = async () => {
-    const res = await getProductRequest();
-    setProductos(res.data);
-  };
-
-  useEffect(() => {
-    fetchStrapi();
-  }, []);
+  const { productos } = useContext(DataContext);
 
   return (
     <div className="w-full h-full grid grid-cols-4  lg:grid-cols-4   md:grid-cols-3  sm:grid-cols-1 p-20">
