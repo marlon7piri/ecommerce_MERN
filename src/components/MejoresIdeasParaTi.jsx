78d63dfd40fd8f39d638881898d56663924aadfc
Marlon Rodriguez Rivera <110213632+marlon7piri@users.noreply.github.com>
2024-04-29 21:09:27 -0500

    add new styles 2

--- src/components/MejoresIdeasParaTi.jsx
+++ src/components/MejoresIdeasParaTi.jsx
@@ -7,14 +7,14 @@ import { Carrusel } from "./Carrusel";
 
 export default function MejoresIdeasParaTi() {
   return (
-    <div className="w-full h-full sm:mt-24 ">
+    <div className="w-full min-h-screen mt-24 ">
       <h2 className="text-center text-4xl text-gray-700">Best deals for you</h2>
       <div className="w-full">
         <div className="flex flex-col items-center justify-center sm:flex sm:gap-4 sm:mb-4 p-4">
           <FaHandsWash className="text-3xl text-gray-900" />
           <p className="text-4xl text-gray-900">hands wash only</p>
         </div>
-        <div className="w-full">
+        <div className="w-full h-full">
           
          <Carrusel/>
         </div>
